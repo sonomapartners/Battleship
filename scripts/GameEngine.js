269d16afd26f0c28d3adc273678729d7d430e45d
William Dibbern <gotdibbs@outlook.com>
2016-11-15 16:19:25 -0600

    First pass of board layout

--- scripts/GameEngine.js
+++ scripts/GameEngine.js
@@ -1,7 +1,12 @@
 (function(global) {
+    'use strict';
 
-    function onLoad() {
-        document.querySelectorAll('[data-hook~="player-list"]').forEach(function (select) {
+    var _coordinateTemplate = _.template('<span class="tag tag-default coordinate" data-hook="x_<%= x %>_y_<%= y %>">&nbsp;</span>');
+
+    function _loadBots() {
+        var playerLists = document.querySelectorAll('[data-hook~="player-list"]');
+        
+        _.each(playerLists, function (select) {
             for (var botName in global.Bots) {
                 var bot = global.Bots[botName],
                     option = document.createElement('option');
@@ -12,6 +17,30 @@
                 select.appendChild(option);
             }
         });
+    }
+
+    function _cleanBoards() {
+        var boards = document.querySelectorAll('[data-hook~="board"]'),
+            row,
+            x, y;
+
+        _.each(boards, function (board) {
+            for (x = 0; x <= 10; x++) {
+                row = document.createElement('div');
+
+                for (y = 0; y <= 10; y++) {
+                    row.innerHTML += _coordinateTemplate({ x: x, y: y});
+                }
+
+                board.appendChild(row);
+            }
+        });
+    }
+
+    function onLoad() {
+        _loadBots();
+
+        _cleanBoards();
     }
 
     global.addEventListener('DOMContentLoaded', onLoad);

9acdaa57d7e0a73985fb1a81bca25f21d759b9bf
William Dibbern <gotdibbs@outlook.com>
2016-12-13 12:45:05 -0600

    Sample output for MyBot

--- MyBot.js
+++ MyBot.js
@@ -4,10 +4,22 @@
     // ships = [2, 3, 3, 4, 5]
     function setupBoard(ships) {
         // TODO: implement
+
+        // Example output below assumes ships = [2, 3, 3, 4, 5]
+        return [
+            { begin: { x: 0, y: 0}, end: { x: 0, y: 1} },
+            { begin: { x: 0, y: 2}, end: { x: 0, y: 4} },
+            { begin: { x: 0, y: 5}, end: { x: 0, y: 7} },
+            { begin: { x: 1, y: 0}, end: { x: 1, y: 3} },
+            { begin: { x: 2, y: 0}, end: { x: 2, y: 4} }
+        ];
     }
 
     function fire(myMoves, theirMoves) {
         // TODO: implement
+
+        // always fires at 0,0
+        return { x: 0, y: 0};
     }
 
     /* 
